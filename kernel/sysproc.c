376a4ab4781da47e72a62408f14c5db745bb7d4b
yui <wujiaheng954@163.com>
2024-04-26 21:25:15 +0800

    Revert "实现lazy allocation(part)"
    This reverts commit 5eb9ce46672dd88feebb94b229a568fb2e66960e.

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,9 +47,8 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  myproc()->sz = myproc()->sz + n;
-  // if(growproc(n) < 0)
-  //   return -1;
+  if(growproc(n) < 0)
+    return -1;
   return addr;
 }
 
